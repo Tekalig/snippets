5d5580775e0f1ff411d439a1ddc0fb51ec4d7821
Tekalig <tekahazi06@gmail.com>
2025-04-17 17:05:33 +0300

    refactor: replace query with db methods for snippet retrieval in edit page

--- src/app/snippets/[id]/edit/page.js
+++ src/app/snippets/[id]/edit/page.js
@@ -1,14 +1,18 @@
 import React from "react";
 import SnippetsEditForm from "@/components/snippetsEditForm";
-import query from "@/utils/db";
+import {db} from "@/db";
+
 
 export default async function snippetEditPage({ params }) {
   const snippetId = Number(params.id);
-  const snippet = await query("select * from snippets where id = $1", [
-    snippetId,
-  ]);
-  if (snippet.length == 0) {
+  const snippet = await db.snippet.findUnique({
+    where: {
+      id: snippetId,
+    },
+  });
+
+  if (!snippet) {
     return notFound();
   }
-  return <SnippetsEditForm snippet={snippet[0]} />;
+  return <SnippetsEditForm snippet={snippet}/>;
 }
