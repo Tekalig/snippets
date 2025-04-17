94b711f901c3ab701561d750125faf22c93fefb1
Tekalig <tekahazi06@gmail.com>
2025-04-17 17:05:25 +0300

    refactor: update NewSnippetPage to use form state management and improve UI elements

--- src/app/snippets/new/page.js
+++ src/app/snippets/new/page.js
@@ -1,43 +1,39 @@
-import React from "react";
-import query from "@/utils/db";
-import { redirect } from "next/navigation";
+'use client'
+import {useFormState} from 'react-dom';
+import * as actions from "../../../actions/index";
 
 export default function NewSnippetPage() {
-  async function handleSubmit(event) {
-    "use server";
-    const title = event.get("title");
-    const code = event.get("code");
-    const response = await query(
-      "INSERT INTO snippets (title, code) VALUES ($1, $2) RETURNING *",
-      [title, code]
-    );
-    redirect("/");
-  }
+  const [formState, action] = useFormState(actions.newSnippet, {message:""})
+
   return (
-    <form action={handleSubmit}>
+    <form action={action} className='w-1/2 mx-auto'>
       <h1 className="font-bold m-3 text-3xl">Create Snippets</h1>
       <div className="flex justify-center flex-col gap-5 cap border rounded-md p-4 shadow-md bg-neutral-300">
         <div className="flex gap-4 flex-wrap">
-          <label htmlFor="title" className="w-12 text-xl">
+          <label htmlFor="title" className="w-12 text-xl font-semibold">
             Title:
           </label>
           <input
             name="title"
-            className="border rounded p-2 w-full"
+            className="border rounded p-4 w-full"
             id="title"
           />
         </div>
         <div className="flex gap-4 flex-wrap">
-          <label htmlFor="code" className="w-12 text-xl">
+          <label htmlFor="code" className="w-12 text-xl font-semibold">
             Code:
           </label>
           <textarea
             name="code"
-            className="border rounded p-2 w-full"
+            className="border rounded p-2 w-full h-36"
             id="code"
           />
         </div>
-        <button type="submit" className="rounded p-2 bg-green-400 text-2xl">
+        {formState.message ? <div className='text-lg text-red-500 animate-pulse'>
+          {formState.message}
+        </div>:null}
+        
+        <button type="submit" className="rounded p-2 bg-green-400 text-2xl w-24 mx-auto capitalize font-extrabold shadow-md hover:bg-green-600">
           save
         </button>
       </div>
